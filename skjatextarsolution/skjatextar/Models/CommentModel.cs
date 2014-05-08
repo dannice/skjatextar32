762a8ce91e01a8e6d31111f6d738cd1498ebbc3a
annamt06 <annamt06@ru.is>
2014-05-08 15:01:17 +0000

    Bætti við models. Á eftir að bæta við kóða inn í sum módelin.

---
+++ skjatextarsolution/skjatextar/Models/CommentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace skjatextar.Models
+{
+    public class CommentModel
+    {
+        public int commentId { get; set; }
+        [Required]
+        public string comment { get; set; }
+        public int report { get; set; }
+        public DateTime commentDate { get; set; }
+
+
+    }
+}
