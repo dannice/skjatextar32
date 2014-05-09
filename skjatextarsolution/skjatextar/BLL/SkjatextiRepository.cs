cb555df94484cc11c1257b69b62ed32b21e6eec3
kristrun12 <kristrun12@ru.is>
2014-05-09 16:08:18 +0000

    tv show listi

--- skjatextarsolution/skjatextar/BLL/SkjatextiRepository.cs
+++ skjatextarsolution/skjatextar/BLL/SkjatextiRepository.cs
@@ -7,6 +7,24 @@ namespace skjatextar.BLL
 {
     public class SkjatextiRepository
     {
+        // Funtion to make a dropdown list.
+        public List<Models.SrtFileModel> GetTvShow()
+        {
+            SkjatextiEntities context = new SkjatextiEntities();
+
+            var list = new List<Models.SrtFileModel>();
+            var query = from it in context.SrtFile
+                        orderby it.title
+                        select it;
+           
+            foreach (var item in query)
+            {
+                var tvName = new Models.SrtFileModel();
+                tvName.title = item.title;
+                list.Add(tvName);
+            }
+            return list;
+        }
 
     }
 }
