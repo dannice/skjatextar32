0395b19013ae4f458bc5cfb961e6ac532df848a8
kristrun12 <kristrun12@ru.is>
2014-05-10 14:19:42 +0000

    drop

--- skjatextarsolution/skjatextar/BLL/SkjatextiRepository.cs
+++ skjatextarsolution/skjatextar/BLL/SkjatextiRepository.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace skjatextar.BLL
 {
+
     public class SkjatextiRepository
     {
         // Funtion to make a dropdown list.
@@ -13,37 +14,64 @@ namespace skjatextar.BLL
             SkjatextiEntities context = new SkjatextiEntities();
 
             var listTitle = new List<Models.SrtFileModel>();
-            
-            var query = from it in context.SrtFile
-                        orderby it.title
-                        select it;      
+            var listEpisode = new List<Models.TvShowModel>();
+
+            var query = (from item in context.SrtFile
+                         join elem in context.TvShow
+                         on item.tvId equals elem.tvId
+                         orderby item.title
+                         select item);      
 
             foreach (var item in query)
             {
-                var tvName = new Models.SrtFileModel();
-                tvName.title = item.title;
-                listTitle.Add(tvName);
+                
+                var srt = new Models.SrtFileModel();
+                var show = new Models.TvShowModel();
+                srt.title = item.title;
+                show.episodeAbout = item.TvShow.episodeAbout;
+                show.season = item.TvShow.season;
+                show.episode = item.TvShow.episode;
+                listTitle.Add(srt);
+                listEpisode.Add(show);
+                //listEpisode.Add(show.episode);
+                //listEpisode.Add(show.season);
+
+                listEpisode.Add(show);
             }
+           // var valueList = listTitle.
             return listTitle;
         }
 
-        public List<Models.TvShowModel> GetTvShowModel()
+        public List<Models.SrtFileModel> GetMovie()
         {
             SkjatextiEntities context = new SkjatextiEntities();
 
-            var listSeason = new List<Models.TvShowModel>();
+            var listTitle = new List<Models.SrtFileModel>();
+            var listMovie = new List<Models.MovieModel>();
 
-            var query = from it in context.TvShow
-                             orderby it.season
-                             select it;
+            var query = (from item in context.SrtFile
+                         join elem in context.Movie
+                         on item.movieId equals elem.movieId
+                         orderby item.title
+                         select item);
 
-            foreach(var item in query)
+            foreach (var item in query)
             {
-                    var tvSeason = new Models.TvShowModel();
-                    tvSeason.season = item.season;
-                    listSeason.Add(tvSeason);
+
+                var srt = new Models.SrtFileModel();
+                var movie = new Models.MovieModel();
+                srt.title = item.title;
+                movie.year = item.Movie.year;
+                listTitle.Add(srt);
+                listMovie.Add(movie);
+                //listEpisode.Add(show.episode);
+                //listEpisode.Add(show.season);
+
+                listMovie.Add(movie);
             }
-            return listSeason;
+            // var valueList = listTitle.
+
+            return listTitle;
         }
     }
 }
