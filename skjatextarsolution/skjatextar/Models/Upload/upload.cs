6832beddde4f33608971e474788a5897a8a4a550
dannice <daniela12@ru.is>
2014-05-11 12:44:19 +0000

    Upload Models

---
+++ skjatextarsolution/skjatextar/Models/Upload/upload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace skjatextar.Models.Upload
+{
+	class upload
+	{
+		[Key]
+		public virtual int Upload_id { get; set; }
+		[Required]
+		public virtual string Title { get; set; }
+	}
+}
